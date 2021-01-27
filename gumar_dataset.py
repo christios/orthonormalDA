2db54c81bda7212afeae033a434e3d9c805232a5
christios <christian-cayralat@hotmail.com>
2021-01-27 17:38:45 +0200

    gumar finalize

--- gumar_dataset.py
+++ gumar_dataset.py
@@ -1,16 +1,16 @@
 import os
-import sys
-import urllib.request
 import zipfile
 import xml.etree.ElementTree as ET
 import numpy as np
+import pyarabic.araby as araby
+import re
 
-# Loads a morphological dataset in a vertical format.
+# Loads a NMT dataset sentence per sentence.
 # - The data consists of three Datasets
-#   - train
-#   - dev
-#   - test
-# - Each dataset is composed of factors (FORMS, LEMMAS, TAGS), each an
+#   - TRAIN
+#   - DEV
+#   - TEST
+# - Each dataset is composed of factors (SOURCE, TARGET), each an
 #   object containing the following fields:
 #   - word_strings: Strings of the original words.
 #   - word_ids: Word ids of the original words (uses <unk> and <pad>).
@@ -19,8 +19,7 @@ import numpy as np
 #   - alphabet_map: Character -> char_id map.
 #   - alphabet: Char_id -> character list.
 #   - charseqs: Sequences of characters of the original words.
-class MorphoDataset:
-    _URL = "https://ufal.mff.cuni.cz/~straka/courses/npfl114/1920/datasets/"
+class GumarDataset:
 
     class Factor:
         PAD = 0
@@ -46,51 +45,32 @@ class MorphoDataset:
             self.charseqs = charseqs
 
     class Dataset:
-        FORMS = 0
-        LEMMAS = 1
-        TAGS = 2
-        FACTORS = 3
+        SOURCE = 0
+        TARGET = 1
+        FACTORS = 2
 
         def __init__(self, data_file, train=None, shuffle_batches=True, add_bow_eow=False, max_sentences=None, seed=42):
             # Create factors
             self._data = []
             for f in range(self.FACTORS):
-                self._data.append(MorphoDataset.Factor(f in [self.FORMS, self.LEMMAS], train._data[f] if train else None))
-
-            in_sentence = False
-            for line in data_file:
-                line = line.decode("utf-8").rstrip("\r\n")
-                if line:
-                    columns = line.split("\t")
-                    for f in range(self.FACTORS):
-                        factor = self._data[f]
-                        if not in_sentence:
-                            if len(factor.word_ids): factor.word_ids[-1] = np.array(factor.word_ids[-1], np.int32)
-                            factor.word_ids.append([])
-                            factor.word_strings.append([])
-                            if factor.characters:
-                                factor.charseqs.append([])
-
-                        word = columns[f]
-                        factor.word_strings[-1].append(word)
-
-                        # Character-level information
-                        if factor.characters:
-                            factor.charseqs[-1].append([])
-                            if add_bow_eow:
-                                factor.charseqs[-1][-1].append(MorphoDataset.Factor.BOW)
-                            for c in word:
-                                if c not in factor.alphabet_map:
-                                    if train:
-                                        c = "<unk>"
-                                    else:
-                                        factor.alphabet_map[c] = len(factor.alphabet)
-                                        factor.alphabet.append(c)
-                                factor.charseqs[-1][-1].append(factor.alphabet_map[c])
-                            if add_bow_eow:
-                                factor.charseqs[-1][-1].append(MorphoDataset.Factor.EOW)
-
-                        # Word-level information
+                self._data.append(GumarDataset.Factor(f in [self.SOURCE, self.TARGET], train._data[f] if train else None))
+            
+            data_xml = ET.parse(data_file).getroot()
+            for sentence in data_xml.getchildren():
+                for f in range(self.FACTORS):
+                    factor = self._data[f]
+                    if len(factor.word_ids): factor.word_ids[-1] = np.array(factor.word_ids[-1], np.int32)
+                    factor.word_ids.append([])
+                    factor.word_strings.append([])
+                    if factor.characters:
+                        factor.charseqs.append([])
+                    
+                    sentence_temp = sentence[f%2].text
+                    sentence_temp = GumarDataset.preprocess(sentence_temp)
+                    sentence_temp = sentence_temp.split(' ')
+                    sentence_temp = [token for token in sentence_temp if token]
+                    # Word-level information
+                    for word in sentence_temp:
                         if word not in factor.words_map:
                             if train:
                                 word = "<unk>"
@@ -98,15 +78,34 @@ class MorphoDataset:
                                 factor.words_map[word] = len(factor.words)
                                 factor.words.append(word)
                         factor.word_ids[-1].append(factor.words_map[word])
+                        factor.word_strings[-1].append(word)
 
-                    in_sentence = True
-                else:
-                    in_sentence = False
-                    if max_sentences is not None and len(self._data[self.FORMS].word_ids) >= max_sentences:
-                        break
+                        # Character-level information
+                        if factor.characters:
+                            factor.charseqs[-1].append([])
+                            if add_bow_eow:
+                                factor.charseqs[-1][-1].append(
+                                    GumarDataset.Factor.BOW)
+                            for c in word:
+                                if c not in factor.alphabet_map:
+                                    if train:
+                                        c = "<unk>"
+                                    else:
+                                        factor.alphabet_map[c] = len(
+                                            factor.alphabet)
+                                        factor.alphabet.append(c)
+                                factor.charseqs[-1][-1].append(
+                                    factor.alphabet_map[c])
+                            if add_bow_eow:
+                                factor.charseqs[-1][-1].append(
+                                    GumarDataset.Factor.EOW)
 
-            self._size = len(self._data[self.FORMS].word_ids)
+                if max_sentences is not None and len(self._data[self.SOURCE].word_ids) >= max_sentences:
+                    break
+
+            self._size = len(self._data[self.SOURCE].word_ids)
             self._shuffler = np.random.RandomState(seed) if shuffle_batches else None
+
 
         @property
         def data(self):
@@ -123,11 +122,11 @@ class MorphoDataset:
                 permutation = permutation[batch_size:]
 
                 batch = []
-                max_sentence_len = max(len(self._data[self.FORMS].word_ids[i]) for i in batch_perm)
+                max_sentence_len = max(len(self._data[self.SOURCE].word_ids[i]) for i in batch_perm)
 
                 # Word-level data
                 for factor in self._data:
-                    batch.append(MorphoDataset.FactorBatch(np.zeros([batch_size, max_sentence_len], np.int32)))
+                    batch.append(GumarDataset.FactorBatch(np.zeros([batch_size, max_sentence_len], np.int32)))
                     for i in range(batch_size):
                         batch[-1].word_ids[i, :len(factor.word_ids[batch_perm[i]])] = factor.word_ids[batch_perm[i]]
 
@@ -145,17 +144,29 @@ class MorphoDataset:
                 yield batch
 
 
+    @staticmethod
+    def preprocess(sentence):
+        sentence = araby.strip_tatweel(sentence)
+        sentence = araby.normalize_alef(sentence)
+        sentence = araby.normalize_ligature(sentence)
+        sentence = araby.normalize_teh(sentence)
+        sentence = re.sub(r'[^\d\w]', r' ', sentence)
+        sentence = re.sub(r'( ){2,}', r'\1', sentence)
+        return sentence
+
+
     def __init__(self, dataset, add_bow_eow=False, max_sentences=None):
         path = "{}.zip".format(dataset)
-        if not os.path.exists(path):
-            print("Downloading dataset {}...".format(dataset), file=sys.stderr)
-            urllib.request.urlretrieve("{}/{}".format(self._URL, path), filename=path)
 
         with zipfile.ZipFile(path, "r") as zip_file:
-            for dataset in ["train", "dev", "test"]:
-                with zip_file.open("{}_{}.txt".format(os.path.splitext(path)[0], dataset), "r") as dataset_file:
-                    setattr(self, dataset, self.Dataset(dataset_file,
-                                                        train=self.train if dataset != "train" else None,
-                                                        shuffle_batches=dataset == "train",
+            for dataset in ["TRAIN", "DEV", "TEST"]:
+                with zip_file.open(f"{os.path.splitext(path)[0]}/{dataset}_annotated_Gumar_corpus.xml", "r") as dataset_file:
+                    setattr(self, dataset.lower(), self.Dataset(dataset_file,
+                                                        train=self.train if dataset != "TRAIN" else None,
+                                                        shuffle_batches=dataset == "TRAIN",
                                                         add_bow_eow=add_bow_eow,
                                                         max_sentences=max_sentences))
+
+
+if __name__ == "__main__":
+    gumar = GumarDataset('annotated-gumar-corpus')
