e1c1f7f1f8a81941fada36c5f604965c82c51313
Christian Cayralat <ccayral@tony-2.coli.uni-saarland.de>
2021-07-02 08:46:54 +0200

    Initial commit segmenter

---
+++ segmentation/dialect_data.py
@@ -0,0 +1,247 @@
+import re
+import regex
+import os
+from collections import Counter
+
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
+from transformers import BertTokenizerFast
+from torch import Tensor
+
+from spell_correct.utils import pad_sents_char, pad_sents
+from spell_correct.utils import AlignmentHandler
+
+class DialectData(Dataset):
+    def __init__(self, args, data, vocab, device) -> None:
+        self.vocab = vocab
+        self.device = device
+        self.args = args
+        self.bert_tokenizer = None
+        if args.use_bert_enc:
+            self.bert_tokenizer = BertTokenizerFast.from_pretrained(
+                args.bert_model, cache_dir=args.bert_cache_dir)
+        
+        self.use_sent_level = args.use_sent_level
+        self.src_raw = [f[0] for f in data]
+        self.tgt_raw = [f[1] for f in data]
+        self.src_char = [f[2] for f in data]
+        self.src_char = pad_sents_char(self.src_char,
+                                       self.vocab.src.char2id['<pad>'],
+                                       max_sent_length=args.max_sent_len,
+                                       max_word_length=args.max_decode_len)
+        self.tgt_char = [f[3] for f in data]
+        self.tgt_char = pad_sents_char(self.tgt_char,
+                                       self.vocab.tgt.char2id['<pad>'],
+                                       max_sent_length=args.max_sent_len,
+                                       max_word_length=args.max_decode_len)
+        if args.use_bert_enc:
+            self.src_bert = [f[4] for f in data]
+            self.src_bert_mask = [f[5] for f in data]
+
+        if args.use_sent_level:
+            self.src_word = [f[6] for f in data]
+            self.lengths_word = [len(sent) for sent in self.src_word]
+            self.src_word = pad_sents(self.src_word,
+                                      self.vocab.src.word2id['<pad>'],
+                                      maxlen=args.max_sent_len)
+            self.tgt_word = [f[7] for f in data]
+            self.tgt_word = pad_sents(self.tgt_word,
+                                      self.vocab.tgt.word2id['<pad>'],
+                                      maxlen=args.max_sent_len)
+
+        assert len(self.src_char) == len(self.src_word) if args.use_sent_level else len(self.src_char) \
+            == len(self.tgt_char) == len(self.tgt_word) if args.use_sent_level else len(self.tgt_char) \
+            == len(self.src_bert) if args.use_bert_enc else len(self.src_char) \
+            == len(self.src_bert_mask) if args.use_bert_enc else len(self.src_char) \
+            == len(self.lengths_word) if args.use_sent_level else len(self.src_char), 'Error in data compilation'
+
+    def __getitem__(self, index):
+        src_bert = getattr(self, 'src_bert', None)
+        src_bert_mask = getattr(self, 'src_bert_mask', None)
+        src_word = getattr(self, 'src_word', None)
+        tgt_word = getattr(self, 'tgt_word', None)
+        lengths_word = getattr(self, 'lengths_word', None)
+        if src_bert:
+            src_bert = src_bert[index]
+            src_bert_mask = src_bert_mask[index]
+        if src_word:
+            src_word = src_word[index]
+            tgt_word = tgt_word[index]
+            lengths_word = lengths_word[index]
+        inputs = dict(src_raw=self.src_raw[index],
+                      src_char=self.src_char[index],
+                      src_word=src_word,
+                      lengths_word=lengths_word,
+                      src_bert=src_bert,
+                      src_bert_mask=src_bert_mask,
+                      tgt_raw=self.tgt_raw[index],
+                      tgt_char=self.tgt_char[index],
+                      tgt_word=tgt_word)
+        return inputs
+
+    def __len__(self):
+        return len(self.src_char)
+
+    def generate_batch(self, data_batch):
+        src_raw_batch, tgt_raw_batch = [], []
+        src_char_batch, src_word_batch = [], []
+        lengths_word_batch = []
+        src_bert_batch, src_bert_mask_batch = [], []
+        tgt_char_batch, tgt_word_batch = [], []
+        for inputs in data_batch:
+            src_raw_batch.append(inputs['src_raw'])
+            src_char_batch.append(inputs['src_char'])
+            tgt_raw_batch.append(inputs['tgt_raw'])
+            tgt_char_batch.append(inputs['tgt_char'])
+            if inputs['src_word']:
+                src_word_batch.append(inputs['src_word'])
+                lengths_word_batch.append(inputs['lengths_word'])
+                tgt_word_batch.append(inputs['tgt_word'])
+            if inputs['src_bert']:
+                src_bert_batch.append(inputs['src_bert'])
+                src_bert_mask_batch.append(inputs['src_bert_mask'])
+
+        src_char_batch = torch.tensor(src_char_batch, dtype=torch.long).permute(
+            1, 0, 2).to(self.device)
+        tgt_char_batch = torch.tensor(tgt_char_batch, dtype=torch.long).permute(
+            1, 0, 2).to(self.device)
+        if src_word_batch:
+            tgt_word_batch = torch.tensor(tgt_word_batch, dtype=torch.long).permute(
+                1, 0).to(self.device)
+            lengths_word_batch = torch.tensor(
+                lengths_word_batch, dtype=torch.long)
+            src_word_batch = torch.tensor(src_word_batch, dtype=torch.long).permute(
+                1, 0).to(self.device)
+        if src_bert_batch:
+            src_bert_batch = torch.tensor(
+                src_bert_batch, dtype=torch.long).to(self.device)
+            src_bert_mask_batch = torch.tensor(
+                src_bert_mask_batch, dtype=torch.long).to(self.device)
+
+        batch = dict(src_raw=src_raw_batch,
+                    src_char=src_char_batch,
+                    src_word=src_word_batch if isinstance(src_word_batch, Tensor) else None,
+                    lengths_word=lengths_word_batch if isinstance(lengths_word_batch, Tensor) else None,
+                     src_bert=src_bert_batch if isinstance(src_bert_batch, Tensor) else None,
+                     src_bert_mask=src_bert_mask_batch if isinstance(src_bert_mask_batch, Tensor) else None,
+                    tgt_raw=tgt_raw_batch,
+                    tgt_char=tgt_char_batch,
+                    tgt_word=tgt_word_batch if tgt_word_batch is not None else None)
+
+        return batch
+
+
+def split_into_src_tgt():
+    with open('/local/ccayral/orthonormalDA/data/coda-corpus/dialects/Cairo.tsv') as f, \
+            open('/local/ccayral/orthonormalDA/data/coda-corpus/cairo_src.txt', 'w') as f_r, \
+            open('/local/ccayral/orthonormalDA/data/coda-corpus/cairo_tgt.txt', 'w') as f_c:
+        for line in f:
+            line = line.split('\t')
+            if not line[0].isnumeric():
+                continue
+            raw, coda = line[3].strip(), line[4].strip()
+            raw = regex.sub(r'[^\P{P}،]+', '', raw)
+            coda = regex.sub(r'[^\P{P}،]+', '', coda)
+            raw = re.sub(r'(\b)(\w+)(\b)', r'\1 \2 \3', raw)
+            raw = re.sub(r'( ){2,}', r'\1', raw)
+            coda = re.sub(r'(\b)(\w+)(\b)', r'\1 \2 \3', coda)
+            coda = re.sub(r'( ){2,}', r'\1', coda)
+            raw = re.split(r'،', raw)
+            coda = re.split(r'،', coda)
+            assert len(raw) == len(coda), 'Wrong splitting of sentences'
+            for r, c in zip(raw, coda):
+                if r not in ['', ' '] and c not in ['', ' ']:
+                    print(r.strip(), file=f_r)
+                    print(c.strip(), file=f_c)
+
+
+def generate_char_dict():
+    char_dict = Counter()
+    for file in os.scandir('/local/ccayral/orthonormalDA/data/coda-corpus/dialects'):
+        with open(file) as f:
+            for line in f:
+                line = line.split('\t')
+                if not line[0].isnumeric():
+                    continue
+                raw, coda = line[3].strip(), line[4].strip()
+                char_dict.update(raw)
+                char_dict.update(coda)
+
+def load_data(args, vocab, device):
+    alignment_handler = AlignmentHandler(already_split=False, n=3)
+    if args.use_bert_enc:
+        bert_tokenizer = BertTokenizerFast.from_pretrained(
+            args.bert_model, cache_dir=args.bert_cache_dir)
+
+    with open(args.data_src) as f_src, open(args.data_tgt) as f_tgt:
+        src_raw = [line.strip() for line in f_src.readlines()]
+        tgt_raw = [line.strip() for line in f_tgt.readlines()]
+    
+    src_word, tgt_word = None, None
+    if args.use_sent_level:
+        src = [line.split() for line in src_raw]
+        tgt = [line.split() for line in tgt_raw]
+        word_ids_src = vocab.src.words2indices(src, add_beg_end=False)
+        word_ids_tgt = vocab.tgt.words2indices(tgt)
+        char_ids_src = vocab.src.words2charindices(src, add_beg_end=False)
+        char_ids_tgt = vocab.tgt.words2charindices(tgt)
+        src_word = word_ids_src[:args.data_size]
+        tgt_word = word_ids_tgt[:args.data_size]
+    else:
+        src, tgt = alignment_handler.merge_split_src_tgt(
+            src_raw, tgt_raw)
+        char_ids_src = vocab.src.words2charindices(src)
+        char_ids_tgt = vocab.tgt.words2charindices(tgt)
+        src_char = char_ids_src[:args.data_size]
+        tgt_char = char_ids_tgt[:args.data_size]
+
+    src_bert, src_bert_mask = None, None
+    if args.use_bert_enc:
+        src_bert = bert_tokenizer([' '.join(sent) for sent in src],
+                                  padding="max_length",
+                                  truncation=True,
+                                  max_length=args.max_sent_len)
+        src_bert, src_bert_mask = src_bert.input_ids, src_bert.attention_mask
+        src_bert = src_bert[:args.data_size]
+        src_bert_mask = src_bert_mask[:args.data_size]
+
+    data = [x for x in [src_raw, tgt_raw, src_char, tgt_char,
+                        src_bert, src_bert_mask, src_word, tgt_word] if x]
+    data = list(zip(*data))
+
+    lengths = [int(len(src_char)*args.train_split),
+                int(len(src_char)*(1-args.train_split))]
+    if sum(lengths) != len(src_char):
+        lengths[0] += len(src_char) - sum(lengths)
+    train_data, dev_data = random_split(data, lengths)
+
+    train_data = DialectData(args, train_data, vocab, device)
+    dev_data = DialectData(args, dev_data, vocab, device)
+
+    train_iter = DataLoader(train_data, batch_size=args.batch_size,
+                                shuffle=True, collate_fn=train_data.generate_batch)
+    dev_iter = DataLoader(dev_data, batch_size=len(dev_data),
+                            collate_fn=dev_data.generate_batch)
+    return train_iter, dev_iter
+
+
+def process_raw_inputs(args, vocab, raw_inputs, device):
+        if args.use_bert_enc:
+            bert_tokenizer = BertTokenizerFast.from_pretrained(
+                args.bert_model, cache_dir=args.bert_cache_dir)
+        src = vocab.src.words2charindices(
+            [[sent[0]] for sent in raw_inputs])
+        tgt = vocab.src.words2charindices(
+            [[sent[1]] for sent in raw_inputs])
+
+        src_bert = bert_tokenizer(raw_inputs,
+                                  padding="max_length",
+                                  truncation=True,
+                                  max_length=args.max_sent_len)
+        src_bert, src_bert_mask = src_bert.input_ids, src_bert.attention_mask
+
+        data = list(zip(src, src_bert, src_bert_mask, tgt))
+        data = DialectData(args, data, vocab, device)
+
+        return DataLoader(data, batch_size=len(data),
+                          collate_fn=data.generate_batch)
---
+++ segmentation/models.py
@@ -0,0 +1,200 @@
+from typing import Tuple
+
+import torch
+from torch import Tensor
+import torch.nn.functional as F
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+
+class Encoder(nn.Module):
+    def __init__(self,
+                 input_dim: int,
+                 emb_dim: int,
+                 enc_hid_dim: int,
+                 dec_hid_dim: int,
+                 num_layers: int = 1,
+                 bert_emb_dim: int = 0,
+                 dropout: float = 0.1):
+        super().__init__()
+
+        self.input_dim = input_dim
+        self.emb_dim = emb_dim
+        self.bert_emb_dim = bert_emb_dim
+        self.enc_hid_dim = enc_hid_dim
+        self.dec_hid_dim = dec_hid_dim
+        self.num_layers = num_layers
+        self.dropout = dropout
+        self.embedding = nn.Embedding(input_dim, emb_dim)
+        self.rnn = nn.LSTM(input_size=emb_dim + bert_emb_dim,
+                           hidden_size=enc_hid_dim,
+                           bidirectional=True,
+                           num_layers=num_layers)
+        self.dropout = nn.Dropout(dropout)
+        self.enc_to_dec = nn.Linear(self.enc_hid_dim*2, self.dec_hid_dim)
+
+    def forward(self,
+                src: Tensor,
+                lengths,
+                bert_encodings=None,
+                hidden_char=None,
+                integrated_gradients: bool = False) -> Tuple[Tensor]:
+        """- src: [src_len, batch_size]
+        - integrated_gradients: if True, then src is a batch of already embedded inputs
+        - 2 is the number of directions (forward/backward)"""
+        src = self.embedding(src) if not integrated_gradients else src
+        if hidden_char is not None:
+            src = torch.cat([src, hidden_char], dim=-1)
+        if bert_encodings is not None and self.bert_emb_dim:
+            src = torch.cat([src, bert_encodings.repeat(src.size(0), 1, 1)], dim=-1)
+            bert_encodings = None
+        # embedded = [src_len, batch_size, cle_dim]
+        embedded = self.dropout(src)
+        # outputs: [src_len, batch_size, 2*rnn_dim] final-layer hidden states
+        # hidden: [2*rnn_layers, batch_size, rnn_dim] is the final hidden state of each layer-direction
+        # hidden: [forward_1, backward_1, forward_2, backward_2, ...]
+        embedded_packed = pack_padded_sequence(
+            embedded, lengths, enforce_sorted=False)
+
+        if bert_encodings is not None:
+            bert_encodings = torch.cat(2*[bert_encodings])
+            bert_encodings = (torch.cat([bert_encodings.clone()] + [torch.zeros_like(bert_encodings) for i in range(self.num_layers - 1)]),
+                              torch.cat([bert_encodings.clone()] + [torch.zeros_like(bert_encodings) for i in range(self.num_layers - 1)]))
+
+        outputs, hidden = self.rnn(embedded_packed, bert_encodings)
+        outputs, _ = pad_packed_sequence(outputs)
+        # hidden[-2, :, :]: [1, batch_size, rnn_dim]
+        h = hidden[0][0, :, :] + hidden[0][1, :, :]
+        c = hidden[1][0, :, :] + hidden[1][1, :, :]
+        for i in range(self.num_layers - 1):
+            h = torch.stack([h, hidden[0][i + 2, :, :] + hidden[0][i + 3, :, :]])
+            c = torch.stack([c, hidden[1][i + 2, :, :] + hidden[1][i + 3, :, :]])
+        hidden = (h, c)
+        #outputs = [src_len, batch_size, rnn_dim * 2] because 2 directions
+        #hidden = [batch_size, rnn_dim]
+        return outputs, hidden, embedded
+
+
+class Attention(nn.Module):
+    def __init__(self,
+                 enc_hid_dim: int,
+                 dec_hid_dim: int,
+                 attn_dim: int):
+        super().__init__()
+
+        self.enc_hid_dim = enc_hid_dim
+        self.dec_hid_dim = dec_hid_dim
+        self.attn_in = (enc_hid_dim * 2) + dec_hid_dim
+        self.attn = nn.Linear(self.attn_in, attn_dim)
+
+    def forward(self,
+                decoder_hidden: Tensor,
+                encoder_outputs: Tensor) -> Tensor:
+        """decoder_hidden: [batch_size, rnn_dim]
+        encoder_outputs: [src_len, batch_size, 2*rnn_dim]"""
+        src_len = encoder_outputs.shape[0]
+        # decoder_hidden: [batch_size, 1, dec rnn_dim]
+        decoder_hidden = torch.sum(decoder_hidden[0], dim=0).unsqueeze(1)
+        # repeated_hidden: [batch_size, src_len, rnn_dim]
+        repeated_decoder_hidden = decoder_hidden.repeat(1, src_len, 1)
+        # encoder_outputs: [batch_size, src_len, 2*rnn_dim]
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        # decoder_encoder: [batch_size, src_len, 3*rnn_dim]
+        decoder_encoder = torch.cat(
+            (repeated_decoder_hidden, encoder_outputs), dim=2)
+        # energy: [batch_size, src_len, attn_dim]
+        energy = torch.tanh(self.attn(decoder_encoder))
+        # attention: [batch_size, src_len]
+        attention = torch.sum(energy, dim=2)
+        return F.softmax(attention, dim=1)
+
+
+class Decoder(nn.Module):
+    def __init__(self,
+                 output_dim: int,
+                 emb_dim: int,
+                 enc_hid_dim: int,
+                 dec_hid_dim: int,
+                 attention: nn.Module,
+                 num_layers: int = 1,
+                 char_emb_dim: int = 0,
+                 dropout: float = 0.1):
+        super().__init__()
+        self.emb_dim = emb_dim
+        self.enc_hid_dim = enc_hid_dim
+        self.dec_hid_dim = dec_hid_dim
+        self.output_dim = output_dim
+        self.dropout = dropout
+        self.attention = attention
+
+        self.embedding = nn.Embedding(output_dim, emb_dim)
+        self.rnn = nn.LSTM(input_size=2*enc_hid_dim + emb_dim + char_emb_dim,
+                           hidden_size=dec_hid_dim,
+                           num_layers=num_layers)
+        self.dropout = nn.Dropout(dropout)
+
+    def _weighted_encoder_rep(self,
+                              decoder_hidden: Tensor,
+                              encoder_outputs: Tensor) -> Tensor:
+        """decoder_hidden: [batch_size, rnn_dim]
+        encoder_outputs: [src_len, batch_size, 2*rnn_dim]"""
+        # a: [batch_size, src_len]
+        a = self.attention(decoder_hidden, encoder_outputs)
+        # a: [batch_size, 1, src_len]
+        a = a.unsqueeze(1)
+        # encoder_outputs: [batch_size, src_len, 2*rnn_dim]
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        # weighted_encoder_rep: [batch_size, 1, 2*rnn_dim] batch-matrix-matrix product
+        weighted_encoder_rep = torch.bmm(a, encoder_outputs)
+        # weighted_encoder_rep: [1, batch_size, 2*rnn_dim]
+        weighted_encoder_rep = weighted_encoder_rep.permute(1, 0, 2)
+        return weighted_encoder_rep, a
+
+    def forward(self,
+                input: Tensor,
+                decoder_hidden: Tensor,
+                encoder_outputs: Tensor,
+                hidden_tgt_char: Tensor = None) -> Tuple[Tensor]:
+        """ input: [batch_size] Note: "one character at a time"
+            decoder_hidden: [batch_size, dec rnn_dim]
+            encoder_outputs: [src_len, batch_size, 2*rnn_dim]"""
+        # input: [1, batch_size]
+        input = input.unsqueeze(0)
+        # embedded: [1, batch_size, cle_dim]
+        embedded = self.embedding(input)
+        if hidden_tgt_char is not None:
+            embedded = torch.cat([embedded, hidden_tgt_char], dim=-1)
+        embedded = self.dropout(embedded)
+        # weighted_encoder_rep: [1, batch_size, 2*rnn_dim]
+        weighted_encoder_rep, attn_weights = self._weighted_encoder_rep(decoder_hidden,
+                                                                        encoder_outputs)
+        # rnn_input: [1, batch_size, 2*rnn_dim + cle_dim]
+        rnn_input = torch.cat((embedded, weighted_encoder_rep), dim=2)
+        # output: [1, batch_size, rnn_dim] tgt_len = 1 (decoding steps)
+        # decoder_hidden: [1, batch_size, rnn_dim]
+        # output == decoder_hidden
+        output, decoder_hidden = self.rnn(rnn_input, decoder_hidden)
+        # embedded: [batch_size, cle_dim]
+        # output: [batch_size, rnn_dim]
+        embedded, output = embedded.squeeze(0), output.squeeze(0)
+        # weighted_encoder_rep: [batch_size, 2*rnn_dim]
+        weighted_encoder_rep = weighted_encoder_rep.squeeze(0)
+        # output = [batch_size, tgt_vocab]
+        output = torch.cat((output,
+                            weighted_encoder_rep,
+                            embedded), dim=1)
+        return output, decoder_hidden, attn_weights.squeeze(1)
+
+
+class Generator(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super().__init__()
+        self.output = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=-1)
+
+    def forward(self, x):
+        # |x| = (batch_size, length, hidden_size)
+        y = self.softmax(self.output(x))
+        # |y| = (batch_size, length, output_size)
+        # Return log-probability instead of just probability.
+        return y
