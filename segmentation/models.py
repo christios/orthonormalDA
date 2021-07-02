d6e7f06757b272d1aaa7fcbfec5ba95f8fd25fb3
Christian Cayralat <ccayral@tony-2.coli.uni-saarland.de>
2021-07-02 09:39:09 +0200

    Eliminate all missing variables

--- segmentation/models.py
+++ segmentation/models.py
@@ -23,15 +23,17 @@ def log_sum_exp(vec):
 
 class BiLSTM_CRF(nn.Module):
 
-    def __init__(self, vocab_size, tag_to_ix, embedding_dim, hidden_dim):
+    def __init__(self, vocab, embedding_dim, hidden_dim):
         super(BiLSTM_CRF, self).__init__()
         self.embedding_dim = embedding_dim
         self.hidden_dim = hidden_dim
-        self.vocab_size = vocab_size
-        self.tag_to_ix = tag_to_ix
-        self.tagset_size = len(tag_to_ix)
+        self.vocab_size = len(vocab.src.char2id)
+        self.tag_to_ix = vocab.src.char2id
+        self.tagset_size = len(self.tag_to_ix)
+        self.START_TAG = vocab.src.start_of_word
+        self.STOP_TAG = vocab.src.end_of_word
 
-        self.word_embeds = nn.Embedding(vocab_size, embedding_dim)
+        self.word_embeds = nn.Embedding(self.vocab_size, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim // 2,
                             num_layers=1, bidirectional=True)
 
@@ -45,8 +47,8 @@ class BiLSTM_CRF(nn.Module):
 
         # These two statements enforce the constraint that we never transfer
         # to the start tag and we never transfer from the stop tag
-        self.transitions.data[tag_to_ix[START_TAG], :] = -10000
-        self.transitions.data[:, tag_to_ix[STOP_TAG]] = -10000
+        self.transitions.data[self.tag_to_ix[self.START_TAG], :] = -10000
+        self.transitions.data[:, self.tag_to_ix[self.STOP_TAG]] = -10000
 
         self.hidden = self.init_hidden()
 
@@ -58,7 +60,7 @@ class BiLSTM_CRF(nn.Module):
         # Do the forward algorithm to compute the partition function
         init_alphas = torch.full((1, self.tagset_size), -10000.)
         # START_TAG has all of the score.
-        init_alphas[0][self.tag_to_ix[START_TAG]] = 0.
+        init_alphas[0][self.tag_to_ix[self.START_TAG]] = 0.
 
         # Wrap in a variable so that we will get automatic backprop
         forward_var = init_alphas
@@ -81,7 +83,7 @@ class BiLSTM_CRF(nn.Module):
                 # scores.
                 alphas_t.append(log_sum_exp(next_tag_var).view(1))
             forward_var = torch.cat(alphas_t).view(1, -1)
-        terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG]]
+        terminal_var = forward_var + self.transitions[self.tag_to_ix[self.STOP_TAG]]
         alpha = log_sum_exp(terminal_var)
         return alpha
 
@@ -97,11 +99,11 @@ class BiLSTM_CRF(nn.Module):
         # Gives the score of a provided tag sequence
         score = torch.zeros(1)
         tags = torch.cat(
-            [torch.tensor([self.tag_to_ix[START_TAG]], dtype=torch.long), tags])
+            [torch.tensor([self.tag_to_ix[self.START_TAG]], dtype=torch.long), tags])
         for i, feat in enumerate(feats):
             score = score + \
                 self.transitions[tags[i + 1], tags[i]] + feat[tags[i + 1]]
-        score = score + self.transitions[self.tag_to_ix[STOP_TAG], tags[-1]]
+        score = score + self.transitions[self.tag_to_ix[self.STOP_TAG], tags[-1]]
         return score
 
     def _viterbi_decode(self, feats):
@@ -109,7 +111,7 @@ class BiLSTM_CRF(nn.Module):
 
         # Initialize the viterbi variables in log space
         init_vvars = torch.full((1, self.tagset_size), -10000.)
-        init_vvars[0][self.tag_to_ix[START_TAG]] = 0
+        init_vvars[0][self.tag_to_ix[self.START_TAG]] = 0
 
         # forward_var at step i holds the viterbi variables for step i-1
         forward_var = init_vvars
@@ -133,7 +135,7 @@ class BiLSTM_CRF(nn.Module):
             backpointers.append(bptrs_t)
 
         # Transition to STOP_TAG
-        terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG]]
+        terminal_var = forward_var + self.transitions[self.tag_to_ix[self.STOP_TAG]]
         best_tag_id = argmax(terminal_var)
         path_score = terminal_var[0][best_tag_id]
 
@@ -144,7 +146,7 @@ class BiLSTM_CRF(nn.Module):
             best_path.append(best_tag_id)
         # Pop off the start tag (we dont want to return that to the caller)
         start = best_path.pop()
-        assert start == self.tag_to_ix[START_TAG]  # Sanity check
+        assert start == self.tag_to_ix[self.START_TAG]  # Sanity check
         best_path.reverse()
         return path_score, best_path
 
